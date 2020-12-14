6f0ad2902061d915e62d93dc3c49d10542783ac6
陈栋明 <chendm0602@163.com>
2020-12-14 10:52:51 +0800

    初始模板

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    我是首页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  },
+  setup(props,content){
+
+    console.log(props,content)
+  }
+}
+</script>
